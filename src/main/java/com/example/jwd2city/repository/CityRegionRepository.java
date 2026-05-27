d4cf8893ffc5399d79071ecf07b1451d5b032959
aw <1541998@qq.com>
2026-05-27 10:43:10 +0800

    去掉运行时需要数据库,直接使用缓存

--- src/main/java/com/example/jwd2city/repository/CityRegionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.jwd2city.repository;
-
-import com.example.jwd2city.entity.CityRegion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CityRegionRepository extends JpaRepository<CityRegion, Integer> {
-}
